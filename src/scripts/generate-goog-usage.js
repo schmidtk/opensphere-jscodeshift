aceac04fd5e85672972a6e01af33397f037fa86d
Kevin Schmidt <kschmidt@caci.com>
2018-06-04 17:00:34 -0600

    Add scripts to generate usage info for Closure library functions.

---
+++ src/scripts/generate-goog-usage.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const jscs = require('jscodeshift');
+
+/**
+ * Find all `goog.*` expressions in a file.
+ * @param {File} file The file being processed.
+ * @param {Object} api The jscodeshift API.
+ * @param {Object} options The jscodeshift options.
+ */
+module.exports = (file, api, options) => {
+  const root = jscs(file.source);
+
+  const expressions = [];
+
+  root.find(jscs.MemberExpression, {
+    object: {name: 'goog'}
+  }).forEach(path => {
+    let parent = path.parent;
+    while (parent) {
+      if (parent.value.type === 'MemberExpression' && parent.value.object.type === 'MemberExpression') {
+        path = parent;
+        parent = path.parent;
+      } else {
+        parent = undefined;
+      }
+    }
+
+    if (path) {
+      delete path.value.comments;
+      delete path.value.leadingComments;
+
+      const srcStr = jscs(path).toSource();
+      if (srcStr.indexOf('/**') !== -1) {
+        console.log(file.path);
+      } else {
+        expressions.push(srcStr);
+      }
+    }
+  });
+
+  if (expressions.length) {
+    fs.appendFile('./goog-usage', `${expressions.join('\n')}\n`, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  }
+
+  return root.toSource();
+};
---
+++ src/scripts/process-goog-usage.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const content = fs.readFileSync(path.resolve(process.cwd(), './goog-usage'), 'utf8').trim();
+const usageList = content.split('\n');
+
+const map = {};
+usageList.forEach(key => {
+  map[key] = map[key] || 0;
+  map[key]++;
+});
+
+const arr = [];
+for (const key in map) {
+  arr.push({
+    key: key,
+    value: map[key]
+  });
+}
+
+const output = arr.sort((a, b) => a.value > b.value ? -1 : a.value < b.value ? 1 : 0)
+    .map(obj => `${obj.value}: ${obj.key}`)
+    .join('\n');
+
+fs.writeFile('./goog-usage', output, function(err) {
+  if(err) {
+    return console.log(err);
+  }
+
+  console.log("The file was saved!");
+});
