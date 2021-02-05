5da759b0212ced43ca44ee285fa2eb177eb97e37
Kevin Schmidt <kschmidt@caci.com>
2021-02-05 11:21:49 -0700

    feat: only process modules, update tests

--- src/transforms/es6/__testfixtures__/closureclass-es6.output.js
+++ src/transforms/es6/__testfixtures__/closureclass-es6.output.js
@@ -1,4 +1,4 @@
-goog.declareModuleId('os.ns.MyClass');
+goog.declareModuleId('os.ns.MyClassTemp');
 
 const os1 = goog.require('os1');
 const ParentClass = goog.require('os.ns.ParentClass');
--- src/transforms/es6/moduletoes6.js
+++ src/transforms/es6/moduletoes6.js
@@ -14,19 +14,20 @@ module.exports = (file, api, options) => {
   const root = jscs(file.source);
   logger.setCurrentFile(file.path);
 
-  // Replace goog.module statement with goog.declareModuleId.
+  // Replace goog.module statement with goog.declareModuleId. Only continue if a module was found.
   const moduleName = replaceModuleWithDeclareModuleId(root);
+  if (moduleName) {
+    // Replace goog 'exports' with ES6 export statements.
+    const isDefault = replaceModuleExportsWithEs6(root);
 
-  // Replace goog 'exports' with ES6 export statements.
-  const isDefault = replaceModuleExportsWithEs6(root);
+    // If the module used a default export, create a shim to preserve backward compatibility.
+    if (isDefault && moduleName && !options.dry) {
+      createEs6Shim(file.path, moduleName);
+    }
 
-  // If the module used a default export, create a shim to preserve backward compatibility.
-  if (isDefault && moduleName && !options.dry) {
-    createEs6Shim(file.path, moduleName);
+    // Remove goog.module.declareLegacyNamespace, if present.
+    removeLegacyNamespace(root);
   }
-
-  // Remove goog.module.declareLegacyNamespace, if present.
-  removeLegacyNamespace(root);
 
   return printSource(root);
 };
