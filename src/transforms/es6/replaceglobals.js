0f1993912792645ae663d399768960c2ab5a8f6c
Kevin Schmidt <kschmidt@caci.com>
2021-05-27 15:00:53 -0600

    fix: ignore shims in replaceglobals script

--- src/transforms/es6/replaceglobals.js
+++ src/transforms/es6/replaceglobals.js
@@ -6,15 +6,17 @@ const {printSource} = require('../../utils/jscs');
 const {logger} = require('../../utils/logger');
 
 module.exports = (file, api, options) => {
+  // Skip shims.
+  if (file.path.endsWith('_shim.js')) {
+    return file.source;
+  }
+
   const root = jscs(file.source);
   logger.setCurrentFile(file.path);
 
-  // Ignore shims.
-  if (!file.path.endsWith('_shim.js')) {
-    convertGlobalRefs(root);
-    convertLegacyRequires(root);
-    sortModuleRequires(root);
-  }
+  convertGlobalRefs(root);
+  convertLegacyRequires(root);
+  sortModuleRequires(root);
 
   return printSource(root);
 };
