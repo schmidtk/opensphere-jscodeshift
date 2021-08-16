8c6c27569c38383a87bbb2ff0878dd4a20addca2
Kevin Schmidt <kschmidt@caci.com>
2021-08-16 07:35:33 -0600

    feat(es6): replace unassigned goog.require statements

--- src/transforms/es6/requiretoimport.js
+++ src/transforms/es6/requiretoimport.js
@@ -5,7 +5,7 @@ const path = require('path');
 const {printSource} = require('../../utils/jscs');
 const {logger} = require('../../utils/logger');
 
-const {createFindMemberExprObject} = require('../../utils/ast');
+const {createFindMemberExprObject, getUniqueVarName} = require('../../utils/ast');
 const {getDependency, getTempModuleName} = require('../../utils/goog');
 
 module.exports = (file, api, options) => {
@@ -16,6 +16,24 @@ module.exports = (file, api, options) => {
   const relativePath = path.relative(workspacePath, file.path);
   const currentProject = relativePath.split(path.sep)[0];
 
+  const getDepPath = (dependency) => {
+    let depPath = dependency.path;
+    if (depPath.startsWith(`${currentProject}/`)) {
+      const fileDir = path.resolve(file.path, '..');
+      const depDir = path.resolve(path.join(workspacePath, dependency.path), '..');
+      const depName = path.basename(dependency.path);
+
+      depPath = path.join(path.relative(fileDir, depDir), depName);
+
+      if (!depPath.startsWith('.')) {
+        depPath = `./${depPath}`;
+      }
+    }
+
+    return depPath;
+  };
+
+  // Replace assigned goog.require statements with imports when referencing an ES module.
   const requireDeclarations = root.find(jscs.VariableDeclaration, {
     declarations: [{
       init: {
@@ -31,23 +49,12 @@ module.exports = (file, api, options) => {
 
     const dependency = getDependency(getTempModuleName(moduleName)) || getDependency(moduleName);
     if (dependency && dependency.moduleType === 'es6') {
-      let depPath = dependency.path;
-      if (depPath.startsWith(`${currentProject}/`)) {
-        const fileDir = path.resolve(file.path, '..');
-        const depDir = path.resolve(path.join(workspacePath, dependency.path), '..');
-        const depName = path.basename(dependency.path).replace('.js', '');
-
-        depPath = path.join(path.relative(fileDir, depDir), depName);
-
-        if (!depPath.startsWith('.')) {
-          depPath = `./${depPath}`;
-        }
-      }
+      const depPath = getDepPath(dependency);
 
       const imports = declarator.id;
       if (imports.type === 'Identifier') {
         // Default import
-        const importDecl = jscs.importDeclaration([jscs.importDefaultSpecifier(imports)], jscs.literal(depPath));
+        const importDecl = jscs.importDeclaration([jscs.importNamespaceSpecifier(imports)], jscs.literal(depPath));
         jscs(requireDecl).replaceWith(importDecl);
       } else if (imports.type === 'ObjectPattern') {
         // Named imports
@@ -62,6 +69,31 @@ module.exports = (file, api, options) => {
     }
   });
 
+  // Replace unassigned goog.require statements with imports when referencing an ES module.
+  const requireExpressions = root.find(jscs.ExpressionStatement, {
+    expression: {
+      type: 'CallExpression',
+      callee: createFindMemberExprObject('goog.require')
+    }
+  });
+
+  requireExpressions.forEach((requireExpr) => {
+    const moduleName = requireExpr.value.expression.arguments[0].value;
+    const dependency = getDependency(getTempModuleName(moduleName)) || getDependency(moduleName);
+    if (dependency && dependency.moduleType === 'es6') {
+      const varName = getUniqueVarName(root, moduleName);
+      const depPath = getDepPath(dependency);
+
+      const importDecl = jscs.importDeclaration([jscs.importNamespaceSpecifier(jscs.identifier(varName))], jscs.literal(depPath));
+
+      // disable eslint for unassigned goog.require statements, because the var will be unused
+      importDecl.comments = [jscs.commentLine(' eslint-disable-line', false, true)];
+
+      jscs(requireExpr).replaceWith(importDecl);
+    }
+  });
+
+  // Update assigned requireType statements to use the expected {default: Thing} syntax for referencing default exports.
   const requireTypeDeclarations = root.find(jscs.VariableDeclaration, {
     declarations: [{
       init: {
